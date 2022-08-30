66f19d13c4d82be9109404490a2cae8a774fcb9f
Elkeno Jones <elkeno.jones@binarystar.ca>
2022-08-30 12:35:20 -0300

    add modal for idea form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { IdeaComponent } from './idea/idea.component';
+import { DialogIdeaFormComponent } from './dialog-idea-form/dialog-idea-form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     TopBarComponent,
-    IdeaComponent
+    IdeaComponent,
+    DialogIdeaFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +27,10 @@ import { IdeaComponent } from './idea/idea.component';
     RouterModule.forRoot([
       { path: '', component: LayoutComponent },
     ]),
+    BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Idea, ideas } from '../idea';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder } from '@angular/forms';
+import { DialogIdeaFormComponent } from '../dialog-idea-form/dialog-idea-form.component';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
 
   ideas: Idea[] = ideas;
+  // ideaForm = this.formBuilder.group({
+  //   id: '',
+  //   title: '',
+  //   description: ''
+  // });
 
-  constructor() {
+  constructor(
+    public dialog: MatDialog,
+    // private formBuilder: FormBuilder
+  ) {}
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogIdeaFormComponent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
   }
 
-  ngOnInit(): void {
-  }
+  // onSubmit(): void {
+  //   // Process checkout data here
+  //   // this.items = this.cartService.clearCart();
+  //   console.warn('Your order has been submitted', this.ideaForm.value);
+  //   // this.checkoutForm.reset();
+  // }
 
 }
+
